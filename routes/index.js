08bcc0ac89b85ba435566c240edf8fd0d7251efb
Stuart Brown <jinky32@gmail.com>
2019-05-23 14:14:53 +0100

    adding simple intent

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+// Import the Dialogflow module from the Actions on Google client library.
+const {dialogflow} = require('actions-on-google');
+
+// Import the firebase-functions package for deployment.
+// const functions = require('firebase-functions');
+
+// Instantiate the Dialogflow client.
+const app = dialogflow({debug: true});
+
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+app.intent('favorite color', (conv, {color}) => {
+    const luckyNumber = color.length;
+    // Respond with the user's lucky number and end the conversation.
+    conv.close('Your lucky number is ' + luckyNumber);
 });
 
-module.exports = router;
+// Set the DialogflowApp object to handle the HTTPS POST request.
+// exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
