b064f90fb68dfe472ec30f5373800c15f66880f2
Stuart Brown <jinky32@gmail.com>
2019-05-24 21:46:44 +0100

    audit chang

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const functions = require("firebase-functions");
-const functions = require("npm audit ");
+const audit = require("npm audit");
 
 // clients
 const dialogFlowApp = require("./DialogflowApp");
