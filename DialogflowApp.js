7f0a08795a2674abf71c1958bbeba9457c8be578
Stuart Brown <jinky32@gmail.com>
2019-05-27 20:29:34 +0100

    loggin payload

--- DialogflowApp.js
+++ DialogflowApp.js
@@ -56,6 +56,7 @@ const {
   app.intent('actions_intent_SIGN_IN', (conv, params, signin) => {
     if (signin.status === 'OK') {
       const payload = conv.user.profile.payload;
+      console.log('THIS IS PAYLOAD ' + payload);
       conv.ask(`I got your account details, ${payload.name}. What do you want to do next?`);
     } else {
       conv.ask(`I won't be able to save your data, but what do you want to do next?`);
