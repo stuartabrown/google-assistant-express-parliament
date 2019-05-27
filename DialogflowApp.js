789f76acecadcd8c5855f75a371720a4ed8efc0e
Stuart Brown <jinky32@gmail.com>
2019-05-27 20:08:01 +0100

    adding sign-in as per https://developers.google.com/actions/identity/google-sign-in

--- DialogflowApp.js
+++ DialogflowApp.js
@@ -1,7 +1,31 @@
+const {dialogflow, SignIn} = require('actions-on-google');
+const app = dialogflow({
+  // REPLACE THE PLACEHOLDER WITH THE CLIENT_ID OF YOUR ACTIONS PROJECT
+  clientId: '397558659346-qdr3pufk60c2t2smhmbapu8jb0egt0i4.apps.googleusercontent.com',
+});
+
+// Intent that starts the account linking flow.
+// app.intent('Start Signin', (conv) => {
+//   conv.ask(new SignIn('To get your account details'));
+// });
+// Create a Dialogflow intent with the `actions_intent_SIGN_IN` event.
+// app.intent('Get Signin', (conv, params, signin) => {
+//   if (signin.status === 'OK') {
+//     const payload = conv.user.profile.payload;
+//     conv.ask(`I got your account details, ${payload.name}. What do you want to do next?`);
+//   } else {
+//     conv.ask(`I won't be able to save your data, but what do you want to do next?`);
+//   }
+// });
+
+
+
+
 const {
     dialogflow,
     Permission,
     Suggestions,
+    SignIn,
     BasicCard
   } = require("actions-on-google");
 
@@ -9,7 +33,10 @@ const {
 
 
   // Instantiate the Dialogflow client.
-  const app = dialogflow({ debug: true });
+  const app = dialogflow({
+    debug: true,
+    clientId: CLIENT_ID
+  });
 
   const getStravaData = async () => {
     const response = await axios("https://www.strava.com/api/v3/activities/2373181785?access_token=ed8c7bfdcac10c2daa7477791cde45b5f51abf5e");
@@ -19,12 +46,22 @@ const {
   app.intent('Default Welcome Intent', async (conv) => {
     const data = await getStravaData();
     console.log(data.athlete.id);
-    conv.ask(new Permission({
-      context: 'Hi there, to get to know you better, you athelete id is '+ data.athlete.id,
-      permissions: 'NAME'
-    }));
+    conv.ask(new SignIn('To get your account details'));
+    // conv.ask(new Permission({
+    //   context: 'Hi there, to get to know you better, you athelete id is '+ data.athlete.id,
+    //   permissions: 'NAME'
+    // }));
   });
 
+
+  app.intent('Get Signin', (conv, params, signin) => {
+    if (signin.status === 'OK') {
+      const payload = conv.user.profile.payload;
+      conv.ask(`I got your account details, ${payload.name}. What do you want to do next?`);
+    } else {
+      conv.ask(`I won't be able to save your data, but what do you want to do next?`);
+    }
+  });
   // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
 // agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
 app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
