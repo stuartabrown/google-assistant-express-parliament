575c1b052f9e132ce08d1a1dfc4edbcc04a47ee4
Stuart Brown <jinky32@gmail.com>
2019-06-04 22:19:37 +0100

    removing stuff from strava project and adding Location intent

--- DialogflowApp.js
+++ DialogflowApp.js
@@ -21,26 +21,18 @@
 const util = require('util');
 
 const {
-    dialogflow,
-    Permission,
-    Suggestions,
-    SignIn,
-    BasicCard
+    dialogflow
   } = require("actions-on-google");
 
-  const axios = require("axios");
+
 
 
   // Instantiate the Dialogflow client.
   const app = dialogflow({
-    debug: true,
-    clientId: '397558659346-qdr3pufk60c2t2smhmbapu8jb0egt0i4.apps.googleusercontent.com',
+    debug: true
   });
 
-  const getStravaData = async () => {
-    const response = await axios("https://www.strava.com/api/v3/activities/2373181785?access_token=ed8c7bfdcac10c2daa7477791cde45b5f51abf5e");
-    return response.data;
-}
+
   app.intent('Default Welcome Intent', (conv) => {
   // app.intent('Default Welcome Intent', async (conv) => {
     // const data = await getStravaData();
@@ -52,37 +44,12 @@ const {
     }));
   });
 
-
-  app.intent('actions_intent_SIGN_IN', (conv, params, signin) => {
-    if (signin.status === 'OK') {
-      const payload = conv.user.profile.payload;
-      console.log('THIS IS PAYLOAD ' + util.inspect(payload, {showHidden: false, depth: null}))
-
-      // console.log('THIS IS PAYLOAD ' + payload);
-      conv.ask(`I got your account details, ${payload.name}. What do you want to do next?`);
-    } else {
-      conv.ask(`I won't be able to save your data, but what do you want to do next?`);
-    }
-  });
-  // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
-// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
-app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
-    if (!permissionGranted) {
-      conv.ask(`Ok, no worries. What's your favorite color?`);
-      conv.ask(new Suggestions('Blue', 'Red', 'Green'));
-    } else {
-
-      console.log(conv.user);
-      conv.data.userName = conv.user.name.display;
-      conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
-      conv.ask(new Suggestions('Blue', 'Red', 'Green'));
-    }
-  });
-
-  app.intent('favorite color', (conv, {color}) => {
-    const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
+// Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+app.intent('Location', (conv, {geo-city}) => {
+  const luckyNumber = geo-city.length;
+  // Respond with the user's lucky number and end the conversation.
+  conv.close('Your lucky number is ' + luckyNumber);
 });
 
   // Handlers go here..
