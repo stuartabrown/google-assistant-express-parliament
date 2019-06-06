94aad349bb30ddd9f205ff1f71d1c6c50cae3f58
Stuart Brown <jinky32@gmail.com>
2019-06-06 21:43:48 +0100

    asking permissions

--- DialogflowApp.js
+++ DialogflowApp.js
@@ -1,8 +1,10 @@
 const util = require('util');
 
 const {
-    dialogflow
-  } = require("actions-on-google");
+  dialogflow,
+  Permission,
+  Suggestions,
+} = require('actions-on-google');
 
 // Instantiate the Dialogflow client.
   const app = dialogflow({
@@ -18,6 +20,31 @@ const {
   //     permissions: 'DEVICE_COARSE_LOCATION'
   //   }));
   // });
+
+// Handle the Dialogflow intent named 'Default Welcome Intent'.
+app.intent('Default Welcome Intent', (conv) => {
+  conv.ask(new Permission({
+    context: 'Hi there, to get to know you better',
+    permissions: [
+      'NAME',
+      'DEVICE_COARSE_LOCATION'
+    ]
+  }));
+});
+
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+  if (!permissionGranted) {
+    conv.ask(`Ok, no worries. What's your favorite color?`);
+    conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+  } else {
+    console.log('conv dot data is ---'+ conv.data);
+    conv.data.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+    conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+  }
+});
 
 // Handle the Dialogflow intent named 'Location'.
 // The intent collects a parameter named 'color'.
