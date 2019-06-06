6545e327533c5148a9eee01d9f07188cddcaeea1
Stuart Brown <jinky32@gmail.com>
2019-06-06 21:57:56 +0100

    correcting location

--- DialogflowApp.js
+++ DialogflowApp.js
@@ -41,7 +41,7 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
   } else {
     console.log('conv dot data is ---'+ conv.data);
     conv.data.userName = conv.user.name.display;
-    conv.data.postcode = conv.user.device.location.zipcode;
+    conv.data.postcode = conv.device.location.zipcode;
     conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color? Postcode is ` + conv.data.postcode);
     conv.ask(new Suggestions('Blue', 'Red', 'Green'));
   }
